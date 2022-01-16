07425a9f84a12716c20adee904668f795c7123e4
Juliana <julianasouzamelo@live.com>
2022-01-16 14:54:35 -0300

    teste

--- src/main/java/com/example/sample/repository/UserOrdersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sample.repository;
-
-import com.example.sample.models.UserOrders;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserOrdersRepository extends JpaRepository<UserOrders, Long> {
-}
