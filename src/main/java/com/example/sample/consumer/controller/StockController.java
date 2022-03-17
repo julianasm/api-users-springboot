85140786afad6859508b28abda03afd651d7c9ec
Juliana <julianasouzamelo@live.com>
2022-03-17 13:57:51 -0300

    limpando code smells

--- src/main/java/com/example/sample/consumer/controller/StockController.java
+++ src/main/java/com/example/sample/consumer/controller/StockController.java
@@ -8,15 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
-@CrossOrigin
+@CrossOrigin(origins = {"http://localhost:8081/", "http://localhost:8083"})
 @RestController
 public class StockController {
 
     @Autowired
     private StockService stockService;
 
-    @CrossOrigin
     @GetMapping("/stocks/{id}")
     public ResponseEntity<StocksDto> getStockbyId(@PathVariable Long id, @RequestHeader("Authorization") String token){
 
@@ -25,7 +23,6 @@ public class StockController {
         return ResponseEntity.ok(stocksDto);
     }
 
-    @CrossOrigin
     @GetMapping("/stock-info/{id}")
     public ResponseEntity<StockInfoDto> getStockInfoById(@PathVariable Long id, @RequestHeader("Authorization") String token){
 
@@ -35,7 +32,7 @@ public class StockController {
     }
 
 
-    @CrossOrigin
+
     @GetMapping("/stocks")
     public ResponseEntity<StockInfoDto[]> getAllStocks(@RequestHeader("Authorization") String token){
 
@@ -45,7 +42,6 @@ public class StockController {
     }
 
 
-    @CrossOrigin
     @PostMapping ("/updateStock")
     public ResponseEntity<StockId> updateStockbyId(@RequestHeader("Authorization") String token){
         return null;
