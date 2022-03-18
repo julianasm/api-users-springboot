687c47adf38518e7b4d6f3ef1217fc68411a69bc
Juliana <julianasouzamelo@live.com>
2022-03-18 16:20:38 -0300

    sonar

---
+++ src/test/java/com/example/sample/OrderServiceTest.java
@@ -0,0 +1,8 @@
+package com.example.sample;
+
+import com.example.sample.service.UserOrderService;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    UserOrderService orderService = new UserOrderService();
+}
